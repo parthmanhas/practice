99735015f45a51e48baa5d4f133942bb2b4a704d
parthmanhas <parthmanhas@gmail.com>
2022-09-17 20:30:45 +0530

    part 2 done but content overflowing

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero/Hero";
 import Navbar from "../components/navbar/Navbar";
+import FeaturedPosts from "../components/featuredPosts/FeaturedPosts";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <FeaturedPosts />
     </>
   );
 };
