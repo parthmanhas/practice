651b07925b300f80c779aa0b047c0159ab7f920e
parthmanhas <parthmanhas@gmail.com>
2022-09-18 16:48:14 +0530

    another section done

--- figma-website/src/home/Home.jsx
+++ figma-website/src/home/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/hero/Hero";
 import Navbar from "../components/navbar/Navbar";
 import FeaturedPosts from "../components/featuredPosts/FeaturedPosts";
 import AboutUs from "../components/aboutUs/AboutUs";
+import ChooseCatergory from "./chooseCategory/ChooseCatergory";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <FeaturedPosts />
       <AboutUs />
+      <ChooseCatergory />
     </>
   );
 };
