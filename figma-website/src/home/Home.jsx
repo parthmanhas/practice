93322fe764271531e5466ad54c4341d6cb49b3dc
parthmanhas <parthmanhas@gmail.com>
2022-09-22 09:41:09 +0530

    fix structure

--- figma-website/src/home/Home.jsx
+++ figma-website/src/home/Home.jsx
@@ -1,14 +1,14 @@
-import Hero from "../components/hero/Hero";
+import Hero from "./hero/Hero";
 import Navbar from "../components/navbar/Navbar";
-import FeaturedPosts from "../components/featuredPosts/FeaturedPosts";
-import AboutUs from "../components/aboutUs/AboutUs";
+import Footer from "../components/footer/Footer";
+import FeaturedPosts from "./featuredPosts/FeaturedPosts";
+import AboutUs from "./aboutUs/AboutUs";
 import ChooseCatergory from "./chooseCategory/ChooseCatergory";
 import SpecialCategory from "./specialCategory/SpecialCategory";
 import Author from "./authors/Author";
 import Logos from "./logos/Logos";
 import Testimonials from "./testimonials/Testimonials";
 import JoinOurTeam from "./joinOurTeam/JoinOurTeam";
-import Footer from "./footer/Footer";
 
 const Home = () => {
   return (
