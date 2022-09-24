25f9366d2b565d0094f8a8abb3d048fdcf5a7d98
parthmanhas <parthmanhas@gmail.com>
2022-09-24 22:48:43 +0530

    added about section 2

--- figma-website/src/about-us/SectionTwo/SectionTwo.tsx
+++ figma-website/src/about-us/SectionTwo/SectionTwo.tsx
@@ -1,7 +1,29 @@
+import './SectionTwo.scss';
+
 const SectionTwo = () => {
   return (
-    <div className='about-us--section-two-container'></div>
-  )
-};  
+    <div className="about-us--section-two-container">
+      <div className="left">
+        <h2>Our team of creatives</h2>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt.
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat.
+        </p>
+      </div>
+      <div className="right">
+        <img src={require('../../static/images/about-us-section-2.jpg')}></img>
+        <div className="box"></div>
+      </div>
+     
+    </div>
+  );
+};
 
 export default SectionTwo;
