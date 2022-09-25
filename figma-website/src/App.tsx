082b2fb4d5405dedfa7db20ce431cb1f9cea16d3
parthmanhas <parthmanhas@gmail.com>
2022-09-25 16:09:46 +0530

    contact page done

--- figma-website/src/App.tsx
+++ figma-website/src/App.tsx
@@ -1,15 +1,10 @@
-import './App.scss';
-import Home from './pages/home/Home';
-import Blog from './pages/blog/Blog';
-import AboutUs from './pages/about-us/AboutUs';
+import "./App.scss";
+import Home from "./pages/home/Home";
+import Blog from "./pages/blog/Blog";
+import AboutUs from "./pages/about-us/AboutUs";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-
-
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import ContactUs from "./pages/contact-us/ContactUs";
 
 const router = createBrowserRouter([
   {
@@ -18,18 +13,20 @@ const router = createBrowserRouter([
   },
   {
     path: "/blog",
-    element: <Blog />
+    element: <Blog />,
   },
   {
     path: "/about-us",
-    element: <AboutUs />
+    element: <AboutUs />,
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs />,
   },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ figma-website/src/pages/contact-us/ContactUs.tsx
@@ -0,0 +1,44 @@
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+import "./Contact.scss";
+
+const ContactUs = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="contact-us-container">
+        <div className="wrapper">
+          <p className="cap-03">Contact us</p>
+          <h1>Let's Start a Conversation</h1>
+          <p className="body-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim.
+          </p>
+          <div className="contact-box">
+            <div className="left">
+              <p className="body-2">Working hours</p>
+              <hr></hr>
+              <h5>Monday To Friday</h5>
+              <h5>9:00 AM to 8:00 PM </h5>
+              <p className="body-1">Our Support Team is available 24/7</p>
+            </div>
+            <div className="right">
+              <p className="body-2">Contact Us</p>
+              <hr></hr>
+              <h5>020 7993 2905</h5>
+              <p className="body-1">hello@finsweet.com</p>
+            </div>
+          </div>
+          <input placeholder="Full Name"></input>
+          <input placeholder="Your Email"></input>
+          <textarea placeholder="Message"></textarea>
+          <button>Send Message</button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default ContactUs;
